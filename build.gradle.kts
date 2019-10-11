ae1d4739157f36f7f8c2f91ab42009bb75c1d485
shion.t.fujie <tonaluna32966@gmail.com>
2019-10-11 19:28:11 +0900

    Prepare the next development

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.50"
+}
+
+group = "com.shionfujie"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+
+    implementation ("io.reactivex.rxjava2:rxjava:2.2.10")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
