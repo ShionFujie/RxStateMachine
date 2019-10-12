344a0b3441a84586e63c037cf866e9252b833b55
shion.t.fujie <tonaluna32966@gmail.com>
2019-10-12 20:31:06 +0900

    buildSrc to manage versions and dependencies

---
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -0,0 +1,7 @@
+object Deps {
+    const val kotlinStdlib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
+
+    const val rxJava = "io.reactivex.rxjava2:rxjava:2.2.10"
+
+    const val junitJupiter = "org.junit.jupiter:junit-jupiter:5.5.2"
+}
